1b62b0611f93ba1adef8d37096d2f63970b5d447
Omry Mendelovich <omry155@gmail.com>
2025-11-10 13:41:22 +0200

    version 0.15.0

--- cortexcli.rb
+++ cortexcli.rb
@@ -1,23 +1,24 @@
-versionStr = "v0.13.0"
-
-class CortexCli < Formula
+class Cortexcli < Formula
   # A brief description of your binary
   desc "Unified CLI for Workloads, APIs and Code scanning"
   # The homepage for your project
   homepage "https://github.com/PaloAltoNetworks/cortexcli"
+  
+  version_str = "v0.15.0"
+  
   # The version number of your binary
-  version versionStr
+  version version_str
   
   # Homebrew will automatically select the correct block based on the user's system.
   on_macos do
     if Hardware::CPU.arm?
       # Apple Silicon (M1, M2, etc.)
-      url "https://github.com/PaloAltoNetworks/cortexcli/releases/download/#{versionStr}/cortexcli-#{versionStr}-arm64.tar.gz"
-      sha256 "<SHA256_CHECKSUM_FOR_THE_ARM_ARCHIVE>"
+      url "https://github.com/PaloAltoNetworks/cortexcli/releases/download/#{version_str}/cortexcli-#{version_str}-arm64.tar.gz"
+      sha256 "cd2630f6d691ea018f8da569ced38413b359bef603e7964df100237dd5920a04"
     elsif Hardware::CPU.intel?
       # Intel Macs
-      url "https://github.com/PaloAltoNetworks/cortexcli/releases/download/#{versionStr}/cortexcli-#{versionStr}-x86_64.tar.gz"
-      sha256 "<SHA256_CHECKSUM_FOR_THE_INTEL_ARCHIVE>"
+      url "https://github.com/PaloAltoNetworks/cortexcli/releases/download/#{version_str}/cortexcli-#{version_str}-x86_64.tar.gz"
+      sha256 "3561ff2b9271c15b2391a0d7d8d00c98be9fd6a15272d82829fadb6cbf0da1a8"
     end
   end
 
@@ -31,6 +32,6 @@ class CortexCli < Formula
   # Optional: Add a test to confirm installation was successful
   test do
     # This test will run `cortexcli --version` and check if the output contains the version
-    assert_match versionStr, shell_output("#{bin}/cortexcli --version")
+    assert_match version_str, shell_output("#{bin}/cortexcli --version")
   end
-ends
+end
